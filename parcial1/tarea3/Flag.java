c2d4b9e0697d22f1c227a7250da45a3885d3c3df
Estudiante-josue <josueleonglez15@gmail.com>
2022-10-04 18:31:52 -0500

    se agrego range.java

---
+++ parcial1/tarea3/Flag.java
@@ -0,0 +1,9 @@
+public class Flag { //name of program
+    public static void main(String[] args) { // mathod
+    	
+        boolean flag = false;
+        System.out.println("valor de flase: ");  // define a bootean var
+        System.out.println(!!flag);    // prints true
+      }
+    
+}
