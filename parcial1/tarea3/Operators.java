697ad9459dc230f0c276c1adcbb765076897243a
Estudiante-josue <josueleonglez15@gmail.com>
2022-10-28 11:14:06 -0500

    tarea3, tarea4

---
+++ parcial1/tarea3/Operators.java
@@ -0,0 +1,25 @@
+import javax.sound.midi.SysexMessage;
+
+public class Operators {
+public static void main(String[] args) {
+    
+    // declare variables
+    double a = 2.4, b = 85;
+    double c = 0;
+// addition operator
+    System.out.println("a + b / 2 = ");
+    c = (a * b) / 2;
+    System.out.println(c);
+   // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
+    // modulo operator
+    System.out.println("a % b = " + (a % b));
+  }
+}
